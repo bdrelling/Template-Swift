faabe53c9555b60e3451bf726065689965e2c4b5
Brian Drelling <brian@drelling.com>
2022-08-27 13:49:10 -0500

    Testing vapor workflow failures

--- Tests/MyModuleTests/MyModuleTests.swift
+++ Tests/MyModuleTests/MyModuleTests.swift
@@ -4,5 +4,10 @@ import XCTest
 final class MyModuleTests: XCTestCase {
     func testSuccess() {
         // do nothing
+
+        // temporarily fail just on macOS for testing.
+        #if os(macOS)
+        XCTFail()
+        #endif
     }
 }
