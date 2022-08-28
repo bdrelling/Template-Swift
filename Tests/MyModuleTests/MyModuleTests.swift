94f17489f12c6e21af1e77aa334ffd4d1c9484d0
Brian Drelling <brian@drelling.com>
2022-08-27 21:06:50 -0500

    Test failure notification from dynamic matrix

--- Tests/MyModuleTests/MyModuleTests.swift
+++ Tests/MyModuleTests/MyModuleTests.swift
@@ -5,10 +5,9 @@ final class MyModuleTests: XCTestCase {
     func testSuccess() {
         // do nothing
 
-        // TODO: Temporarily not failing again!
         // temporarily fail just on macOS for testing.
         #if os(macOS) && swift(>=5.6)
-        // XCTFail()
+        XCTFail()
         #endif
     }
 }
