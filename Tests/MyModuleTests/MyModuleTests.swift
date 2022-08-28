b4e30beb1e25c127252b64f595d36d4935355d0b
Brian Drelling <brian@drelling.com>
2022-08-27 21:24:57 -0500

    Run all tests

--- Tests/MyModuleTests/MyModuleTests.swift
+++ Tests/MyModuleTests/MyModuleTests.swift
@@ -4,10 +4,5 @@ import XCTest
 final class MyModuleTests: XCTestCase {
     func testSuccess() {
         // do nothing
-
-        // temporarily fail just on macOS for testing.
-        #if os(macOS) && swift(>=5.6)
-        XCTFail()
-        #endif
     }
 }
