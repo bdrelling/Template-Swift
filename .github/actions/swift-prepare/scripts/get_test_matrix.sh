6ff1b0c68692151b9245bec6d69e011242b03c3d
Brian Drelling <brian@drelling.com>
2022-08-27 19:00:51 -0500

    Added another action

---
+++ .github/actions/swift-prepare/scripts/get_test_matrix.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+matrix='[]'
+
+# Echo our Xcode version as GitHub Actions step output.
+echo "::set-output name=test-matrix::${matrix}"
--- Tests/MyModuleTests/MyModuleTests.swift
+++ Tests/MyModuleTests/MyModuleTests.swift
@@ -5,9 +5,10 @@ final class MyModuleTests: XCTestCase {
     func testSuccess() {
         // do nothing
 
+        // TODO: Temporarily not failing again!
         // temporarily fail just on macOS for testing.
         #if os(macOS) && swift(>=5.6)
-        XCTFail()
+        // XCTFail()
         #endif
     }
 }
