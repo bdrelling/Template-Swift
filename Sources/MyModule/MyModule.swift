99bc786da0ffaeaf7f633f69bad90174da2bc4fc
Brian Drelling <brian@drelling.com>
2022-09-13 17:00:45 -0500

    Ran SwiftFormat

--- Sources/MyModule/MyModule.swift
+++ Sources/MyModule/MyModule.swift
@@ -1,2 +1,4 @@
+// Copyright © 2022 Brian Drelling. All rights reserved.
+
 // We add a Placeholder to provide a Code Coverage target.
 struct MyModule {}
--- Tests/MyModuleTests/MyModuleTests.swift
+++ Tests/MyModuleTests/MyModuleTests.swift
@@ -1,3 +1,5 @@
+// Copyright © 2022 Brian Drelling. All rights reserved.
+
 import MyModule
 import XCTest
 
